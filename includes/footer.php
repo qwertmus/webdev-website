8619927f20deb98a12e1b189d617805f2896f957
qwertmus <c3025664@hallam.shu.ac.uk>
2023-12-27 21:24:46 +0000

    day1

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+<?php
+$year = date("y");
+?>
+
+<div class="footerContainer">
+<footer>
+      <nav>
+        <menu>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+        </menu>
+      </nav>
+      <div>&copy;<?php echo $year; ?></div>
+    </footer>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+
+<div class="headerContainer">
+	<header>
+        <div class="topBar">
+          <div>
+            <h1>SHU Films</h1>
+          </div>
+          
+          <nav class="mainNav">
+            <menu>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="catalogue.php">Catalogue</a></li>
+              <li><a href="search.php">Search</a></li>
+              <li><a href="contact.php">Contact Us</a></li>
+            </menu>
+          </nav>
+
+    </header>
+</div>
