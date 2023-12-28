89271827325e948fd77e68cf7726ac8a783cb008
qwertmus <c3025664@hallam.shu.ac.uk>
2023-12-28 18:55:31 +0000

    day2 finish

--- info_staff.php
+++ info_staff.php
@@ -14,44 +14,65 @@
 <body>
 <?php
     include("includes/header.php");
-    ?>
-<div class="container">
-    <div class="InfoSection">
-    News<br>
-    Cantor College recently hosted the Software Engineering student of the year awards. Shortlisted candidates from around the country attended the one day event.<br>
-    </div>
-    <div class="InfoSection">
-    Academic Registry<br>
-    For academic regulations, assessment, awards, student records and course validation.<br>
-    </div>
-    <div class="InfoSection">
-    Catering Services<br>
-    Contact the Catering Services regarding on and off-site catering.<br>
-    </div>
-    <div class="InfoSection">
-    Financial Services<br>
-    The financial team based on the 2nd Floor are responsible for all areas of student finance as 
-    well as College budgeting.<br>
-    </div>
-    <div class="InfoSection"> 
-    Information Systems Services<br>
-    The ISS team delivers the College’s computing facilities including all hardware and software. 
-    They also run the staff helpdesk.<br>
-    </div>
-    <div class="InfoSection">
-    Marketing Services<br>
-    The Marketing Team will help promote events and new courses. They will help with press 
-    release preparation.<br>
-    </div>
-    <div class="InfoSection">
-    Personnel Services<br>
-    All staff pay and conditions enquiries should be directed to the Personnel Services team on 
-    the 3rd floor.<br>
-    </div>
-    <div class="InfoSection">
-    Facilities<br>
-    The facilities are responsible for the general care and maintenance of the College. All 
-    enquiries via the main helpdesk.<br>
+?>
+<div class="container_staff">
+        <div class="InfoSectionColumn">
+            <div class="InfoSection">
+
+            </div>
+        </div>  
+    
+        <div class="InfoSectionColumn">
+            <div class="InfoSection">
+            <h1 class="headliner">News</h1><br>
+            Cantor College recently hosted the Software Engineering student of the year awards. Shortlisted candidates from around the country attended the one day event.<br>
+            </div>
+
+            <div class="InfoSection">
+            <h1 class="headliner">Academic Registry</h1><br>
+            For academic regulations, assessment, awards, student records and course validation.<br>
+            </div>
+
+            <div class="InfoSection">
+            <h1 class="headliner">Catering Services</h1><br>
+            Contact the Catering Services regarding on and off-site catering.<br>
+            </div>
+
+            <div class="InfoSection">
+            <h1 class="headliner">Financial Services</h1><br>
+            The financial team based on the 2nd Floor are responsible for all areas of student finance as 
+            well as College budgeting.<br>
+            </div>
+
+            <div class="InfoSection"> 
+            <h1 class="headliner">Information Systems Services</h1><br>
+            The ISS team delivers the College’s computing facilities including all hardware and software. 
+            They also run the staff helpdesk.<br>
+            </div>
+
+            <div class="InfoSection">
+            <h1 class="headliner">Marketing Services</h1><br>
+            The Marketing Team will help promote events and new courses. They will help with press 
+            release preparation.<br>
+            </div>
+
+            <div class="InfoSection">
+            <h1 class="headliner">Personnel Services</h1><br>
+            All staff pay and conditions enquiries should be directed to the Personnel Services team on 
+            the 3rd floor.<br>
+            </div>
+
+            <div class="InfoSection">
+            <h1 class="headliner">Facilities</h1><br>
+            The facilities are responsible for the general care and maintenance of the College. All 
+            enquiries via the main helpdesk.<br>
+            </div>
+        </div>
+    
+    <div class="InfoSectionColumn">
+        <div class="InfoSection">
+            
+        </div>
     </div>
 </div>
 </body>
